ccd79d6836c11a5a1fd55f86df75719d868d30b9
ahmadmusyadada <ahmadmusyadadaminullah@gmail.com>
2019-02-13 21:02:48 +0700

    Get Title

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
+  title = 'Belajar Angular';
+  getTitle(){
+    return this.title;
+  }
+  
   constructor() { }
 
   ngOnInit() {
