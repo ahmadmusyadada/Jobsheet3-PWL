feef1477523d3e5615e3538154415155f9f85d07
ahmadmusyadada <ahmadmusyadadaminullah@gmail.com>
2019-02-13 22:41:46 +0700

    Tugas 1

--- src/app/courses.service.ts
+++ src/app/courses.service.ts
@@ -7,13 +7,21 @@ export class CoursesService {
 
   constructor() { }
 
+  // getCourses(){
+  //   return [
+  //     {id: 0, name: 'HTML'},
+  //     {id: 1, name: 'PHP'},
+  //     {id: 2, name: 'Angular'},
+  //     {id: 3, name: 'C#'},
+  //     {id: 4, name: 'VB.net'},
+  //   ]
+  // }
+
   getCourses(){
     return [
-      {id: 0, name: 'HTML'},
-      {id: 1, name: 'PHP'},
-      {id: 2, name: 'Angular'},
-      {id: 3, name: 'C#'},
-      {id: 4, name: 'VB.net'},
+      {name: 'Author1'},
+      {name: 'Author2'},
+      {name: 'Author3'},
     ]
   }
 }
